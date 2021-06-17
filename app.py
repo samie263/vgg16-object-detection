49f24c0d60348e4059770df8a11f4bc7be24c230
samuel_fyntan <samuelfyntan@gmail.com>
2021-06-17 11:18:09 +0200

    styled index page

--- app.py
+++ app.py
@@ -10,24 +10,10 @@ import pickle
 
 
 app = Flask(__name__)
-#model = pickle.load(open('model.pkl', 'rb'))
-posts = [
-         {
-             'name':'samuel faindani',
-             'title': 'Mr'
-        },
-         {
-             'name':'jerferson henry',
-             'title': 'Mr'
-        }
-     ]
-
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', posts= posts)
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', debug=False)
-    app.run(host='localhost', debug=False)
+    app.run(host='localhost', debug=True)
