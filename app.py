20796a7b329036a2fef1ce622a0ea032b11f7126
samie263 <samuel.faindani@uofzmail.uz.ac.zw>
2021-06-18 23:32:50 +0200

    resolved merge conflicts

--- app.py
+++ app.py
@@ -14,16 +14,11 @@ from model import search_frames, split_to_frames
 UPLOAD_FOLDER = 'static/video_frames'
 import os
 app = Flask(__name__)
-<<<<<<< HEAD
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-=======
->>>>>>> f24eb7f465b455337c9d4b75e14f3d4d3f900a8a
-@app.route('/')
 def index():
     return render_template('index.html')
-<<<<<<< HEAD
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_video():
     if request.method == 'POST':
@@ -31,8 +26,6 @@ def upload_video():
       f.save(secure_filename(f.filename))
       
 @app.route('/search', methods=['GET', 'POST'])
-=======
->>>>>>> f24eb7f465b455337c9d4b75e14f3d4d3f900a8a
 
 def search():
     # getting input with name = fname in HTML form
@@ -42,8 +35,5 @@ def search():
     return render_template('search.html', frames_list=list_frames)
 		
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(host='localhost', debug=True)
-=======
     app.run(host='localhost', debug=True)
->>>>>>> f24eb7f465b455337c9d4b75e14f3d4d3f900a8a
