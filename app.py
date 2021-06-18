22a4c1db6245e4f448970312a758df2806484f49
mkmagaya <makomagaya05@gmail.com>
2021-06-18 22:19:39 +0200

    deleted and modified files

--- app.py
+++ app.py
@@ -5,15 +5,35 @@ Created on Wed Jun 16 14:58:54 2021
 @author: samuel
 """
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from glob import glob
 import pickle
+from model import search_frames, split_to_frames
 
-
+UPLOAD_FOLDER = 'static/video_frames'
+import os
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_video():
+    if request.method == 'POST':
+      f = request.files['videoInput']
+      f.save(secure_filename(f.filename))
+      
+@app.route('/search', methods=['GET', 'POST'])
+
+def search():
+    # getting input with name = fname in HTML form
+    search_text = request.form.get("searchInput")
+    list_frames =  search_frames(search_text)
+    #video = os.listdir('static/video_frames')
+    return render_template('search.html', frames_list=list_frames)
+		
 if __name__ == '__main__':
     app.run(host='localhost', debug=True)
---
+++ model.py
@@ -0,0 +1,5 @@
+def search_frames(string):
+    return ['frame1', 'frame2', 'frame3']
+
+def split_to_frames(video):
+    return video
